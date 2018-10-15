3b9012038b934d73c2e0342fe9785e699a5a980b
labrynto <ddra16@student.aau.dk>
2018-10-15 13:26:06 +0200

    balance fix #1

--- gamemode/sh_player.lua
+++ gamemode/sh_player.lua
@@ -1,14 +1,14 @@
 local meta = FindMetaTable("Player")
 function meta:SetBalance()
 	self:SetNetworkedInt( "Balance", amount )
-	self:SaveMoney()
+	self:SaveBalance()
 end
 function meta:GetBalance()
 	return self:GetNetworkedInt( "Balance" )
 end
 function meta:AddBalance(amount)
-	local current_cash = self:GetMoney()
-	self:SetMoney( current_cash + amount )
+	local currentBalance = self:GetMoney()
+	self:SetBalance( currentBalance + amount )
 end
 function meta:TakeBalance(amount)
     self:AddMoney(-amount)
