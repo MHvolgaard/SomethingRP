15d9f40a6f798b2fb0d8963673fd4e4579418db2
labrynto <ddra16@student.aau.dk>
2018-10-15 12:46:20 +0200

    player class

---
+++ gamemode/player.lua
@@ -0,0 +1,23 @@
+local name = player:Nick()
+local alias = "Leeroy Jenkins"
+local balance
+local job
+local jobDescription = "Citizen"
+local stamina = 100
+local armor = 0
+
+
+
+function ChangeAlias(ply, s) 
+    alias = s
+end
+
+function ChangeJob(ply)
+
+end
+
+function ChangeJobDescription(ply, s)
+    jobDescription = s
+end
+
+
